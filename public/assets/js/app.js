1a1e88ec118945ac5afc1acfe551e71099f19e04
louisqvo <louisqvo@gmail.com>
2020-05-10 14:05:30 -0400

    cart page and add to cart button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,4 @@
+//list of products
 var items = {
     fruitName: [
         "Baby Jack Fruit","Cooking Banana","Dragon Fruit","Durian","Elephant Mango","Hoa Loc Mango","Jack Fruit","Jujubee",
@@ -37,24 +38,138 @@ var items = {
     ]
 }
 
+//Global variable
+var cart = [];
 
+
+//when the page opens first thing the user will see
 $(document).ready(function(){
-    for(var i = 0; i < items.fruitName.length; i++){
-        var image = $(`<img src="public/assets/img/fruits/${items.fruitPic[i]}.png" class="img-fluid" alt="Responsive image">`)
-        var Name = $(`<p>${items.fruitName[i]} - ${items.fruitNameViet[i]}</p>`);
+    $(`.cartTable`).hide();
+    // for(var i = 0; i < 1; i++){
+        for(var i = 0; i < items.fruitName.length; i++){
+            var itemBox = $(`<div class="card" style="width: 18rem;">`)
+            
+            var itemBody = $(`<div class="card-body">`)
 
-        $("#content").append(image);
-        $("#content").append(Name);
+            var image = $(`<img src="public/assets/img/fruits/${items.fruitPic[i]}.png" class="card-img-top" alt="Responsive image">`)
+            var itemName = $(`<h5>${items.fruitName[i]} - ${items.fruitNameViet[i]}</h5>`);
+            var itemMatch = items.fruitName[i].split(' ').join('');
+            var amtInp = $(`<input type="number" class="form-control ${itemMatch}" placeholder="Amount of Box" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+            var addBtn = $(`<button type="button" class="btn btn-success addToCart" data-item="${items.fruitName[i]} - ${items.fruitNameViet[i]}" data-match="${items.fruitName[i]}" data-type="fruit">Add to Cart</button>`);
+
+            itemBody.append(itemName);
+            itemBody.append(amtInp);
+            itemBody.append(addBtn);
+
+            itemBox.append(image);
+            itemBox.append(itemBody);
+
+
+            $("#item").append(itemBox);
+        }
+})
+
+// This function will show the user all fruits when they click the fruit button 
+$('.fruitBtn').on('click', function() {
+    $(`.cartTable`).hide();
+    $('#item').empty();
+    // for(var i = 0; i < 1; i++){
+        for(var i = 0; i < items.fruitName.length; i++){
+            var itemBox = $(`<div class="card" style="width: 18rem;">`)
+            
+            var itemBody = $(`<div class="card-body">`)
+
+            var image = $(`<img src="public/assets/img/fruits/${items.fruitPic[i]}.png" class="card-img-top" alt="Responsive image">`)
+            var itemName = $(`<h5>${items.fruitName[i]} - ${items.fruitNameViet[i]}</h5>`);
+            var itemMatch = items.fruitName[i].split(' ').join('');
+            var amtInp = $(`<input type="number" class="form-control ${itemMatch}" placeholder="Amount of Box" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+            var addBtn = $(`<button type="button" class="btn btn-success addToCart" data-item="${items.fruitName[i]} - ${items.fruitNameViet[i]}" data-match="${items.fruitName[i]}" data-type="fruit">Add to Cart</button>`);
+
+            itemBody.append(itemName);
+            itemBody.append(amtInp);
+            itemBody.append(addBtn);
+
+            itemBox.append(image);
+            itemBox.append(itemBody);
+
+
+            $("#item").append(itemBox);
+        }
+})
+
+// This function will show the user all vegetable when they click the vegetable button 
+$('.vegBtn').on('click', function() {
+    $(`.cartTable`).hide();
+    $('#item').empty();
+    // for(var i = 0; i < 1; i++){
+            for(var i = 0; i < items.vegetableName.length; i++){
+        var itemBox = $(`<div class="card" style="width: 18rem;">`)
+            
+            var itemBody = $(`<div class="card-body">`)
+
+            var image = $(`<img src="public/assets/img/vegetables/${items.vegetablePic[i]}.png" class="card-img-top" alt="Responsive image">`)
+            var itemName = $(`<h5>${items.vegetableName[i]} - ${items.vegetableNameViet[i]}</h5>`);
+            var itemMatch = items.vegetableName[i].split(' ').join('');
+            var amtInp = $(`<input type="number" class="form-control ${itemMatch}" placeholder="Amount in Pounds" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+            var addBtn = $(`<button type="button" class="btn btn-success addToCart" data-item="${items.vegetableName[i]} - ${items.vegetableNameViet[i]}" data-match="${items.vegetableName[i]}" data-type="vegetable">Add to Cart</button>`);
+
+            itemBody.append(itemName);
+            itemBody.append(amtInp);
+            itemBody.append(addBtn);
+
+            itemBox.append(image);
+            itemBox.append(itemBody);
+
+
+            $("#item").append(itemBox);
+
+    }
+})
+
+//function to capture the amount of items the user wants to add to cart and change cart total
+$(document).on('click', 'button.addToCart',function() {
+    var item = $(this).attr('data-item');
+    var preAmo = $(this).attr('data-match').split(' ').join('');
+    var type = $(this).attr('data-type')
+    var amount = $("." + preAmo).val();
+    var totAmount = 0;
+
+    var addToCart = {"type": type, "product" : item, "quantity" : amount};
+
+    cart.push(addToCart);
+    console.log(cart)
+    $("." + preAmo).val('');
+
+    for(var i = 0; i < cart.length; i++){
+        totAmount += parseInt(cart[i]["quantity"]);
+    }
+    $('.cartBtn').text('Cart ' + totAmount)
+})
+
+//this function will show the page when the user clicks on the Cart
+$('.cartBtn').on('click', function() {
+    $(`tbody`).empty();
+    $(`#custInfo`).empty();
+    $(`.cartTable`).show();
+    $('#item').empty();
+
+    var companyName = $(`<input type="text" class="form-control " placeholder="Company Name" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+    var address = $(`<input type="text" class="form-control " placeholder="Address" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+    var phone = $(`<input type="number" class="form-control " placeholder="Phone" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+    var email = $(`<input type="email" class="form-control " placeholder="Email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`);
+    var submit = $(`<button type="button" class="btn btn-success">Submit</button>`);
+
+    for(var i = 0; i < cart.length; i++){
+        var row = $(`<tr>`)
+        var col = $(`<td>${i + 1}</td><td>${cart[i]['type']}</td><td>${cart[i]['product']}</td><td>${cart[i]['quantity']}</td>`)
+
+        row.append(col)
+        $(`tbody`).append(row);
     }
 
-    for(var i = 0; i < items.vegetableName.length; i++){
-        var image = $(`<img src="public/assets/img/vegetables/${items.vegetablePic[i]}.png" class="img-fluid" alt="Responsive image">`)
-        var Name = $(`<p>${items.vegetableName[i]} - ${items.vegetableNameViet[i]}</p>`);
-
-        $("#content").append(image);
-        $("#content").append(Name);
-    }
-
-
-});
-
+    $(`#custInfo`).append(companyName)
+    $(`#custInfo`).append(address)
+    $(`#custInfo`).append(phone)
+    $(`#custInfo`).append(email)
+    $(`#custInfo`).append(submit)
+})
